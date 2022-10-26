d0ad3be12d70a84df8757f286a1c7fa9e8949c7f
sktp06 <kantaporn_s@cmu.ac.th>
2022-10-26 20:13:46 +0700

    init commit

---
+++ src/main/java/se331/rest/Se3312022ProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package se331.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se3312022ProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Se3312022ProjectBackendApplication.class, args);
+    }
+
+}
