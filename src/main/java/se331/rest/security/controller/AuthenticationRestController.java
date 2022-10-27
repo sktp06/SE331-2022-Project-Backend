933b40f6580180d6c064f905effa866d3ba0485c
markwst <Wongsathorn_W@gmail.com>
2022-10-27 18:07:24 +0700

    first Init

---
+++ src/main/java/se331/rest/security/controller/AuthenticationRestController.java
@@ -0,0 +1,140 @@
+package se331.rest.security.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import se331.rest.entity.Organizer;
+import se331.rest.repository.OrganizerRepository;
+import se331.rest.security.entity.Authority;
+import se331.rest.security.entity.AuthorityName;
+import se331.rest.security.entity.JwtUser;
+import se331.rest.security.entity.User;
+import se331.rest.security.repository.AuthorityRepository;
+import se331.rest.security.repository.UserRepository;
+import se331.rest.security.util.JwtTokenUtil;
+import se331.rest.util.LabMapper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+@Slf4j
+public class AuthenticationRestController {
+
+    @Value("${jwt.header}")
+    private String tokenHeader;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthorityRepository authorityRepository;
+
+    @Autowired
+    OrganizerRepository organizerRepository;
+
+
+
+    @PostMapping("${jwt.route.authentication.path}")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest) throws AuthenticationException {
+        log.info("calling Authentication");
+        // Perform the security
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getUsername(),
+                        authenticationRequest.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        log.info("check if it reached?");
+        // Reload password post-security so we can generate token
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        Map result = new HashMap();
+        result.put("token", token);
+        User user = userRepository.findById(((JwtUser) userDetails).getId()).orElse(null);
+        if (user.getOrganizer() != null) {
+            result.put("user", LabMapper.INSTANCE.getOrganizerAuthDTO( user.getOrganizer()));
+        }
+
+        return ResponseEntity.ok(result);
+    }
+
+
+    @GetMapping(value = "${jwt.route.authentication.refresh}")
+    public ResponseEntity<?> refreshAndGetAuthenticationToken(HttpServletRequest request) {
+        String token = request.getHeader(tokenHeader);
+        String username = jwtTokenUtil.getUsernameFromToken(token);
+        JwtUser user = (JwtUser) userDetailsService.loadUserByUsername(username);
+
+        if (jwtTokenUtil.canTokenBeRefreshed(token, user.getLastPasswordResetDate())) {
+            String refreshedToken = jwtTokenUtil.refreshToken(token);
+            return ResponseEntity.ok(new JwtAuthenticationResponse(refreshedToken));
+        } else {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    @PostMapping("/register")
+    @Transactional
+    public ResponseEntity<?> register(@RequestBody User request){
+
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        Authority authAdmin = Authority.builder().name(AuthorityName.ROLE_ADMIN).build();
+
+        authorityRepository.save(authAdmin);
+
+        User user = User.builder()
+                .email(request.getEmail())
+                .firstname("")
+                .lastname("")
+                .username(request.getUsername())
+                .password(encoder.encode(request.getPassword()))
+                .enabled(true)
+                .lastPasswordResetDate(Date.from(LocalDate.of(2021, 01, 01).atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                .build();
+        Organizer organizer = Organizer.builder()
+                .name(user.getFirstname() + " " + user.getLastname())
+                .build();
+
+        organizer.setUser(user);
+        user.setOrganizer(organizer);
+        organizerRepository.save(organizer);
+
+
+
+        user.getAuthorities().add(authAdmin);
+        userRepository.save(user);
+        return ResponseEntity.ok("Register success!!");
+    }
+
+
+}
---
+++ src/main/java/se331/rest/util/LabMapper.java
@@ -0,0 +1,26 @@
+package se331.rest.util;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import se331.rest.entity.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(imports = Collectors.class)
+public interface LabMapper {
+    LabMapper INSTANCE = Mappers.getMapper(LabMapper.class);
+
+    EventDTO getEventDto(Event event);
+
+    List<EventDTO> getEventDto(List<Event> events);
+
+    OrganizerDTO getOrganizerDTO(Organizer organizer);
+
+    List<OrganizerDTO> getOrganizerDTO(List<Organizer> organizers);
+
+    @Mapping(target = "authorities",
+            expression = "java(organizer.getUser().getAuthorities().stream().map(auth -> auth.getName().name()).collect(Collectors.toList()))")
+    OrganizerAuthDTO getOrganizerAuthDTO(Organizer organizer);
+}
